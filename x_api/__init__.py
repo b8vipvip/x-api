4b2fb356152cd4406886d6367da365b8da1e1c81
b8vipvip <917931227@qq.com>
2026-08-14 16:15:18 +0800

    feat: export reusable console router

--- x_api/__init__.py
+++ x_api/__init__.py
@@ -1,3 +1,5 @@
+from .console import create_console_router
+from .fastapi_admin import create_provider_router
 from .gateway import GatewayResult, complete, model_candidates, protocol_candidates, stream_chat
 from .store import DEFAULT_PROTOCOLS, ProviderStore, mask_key, normalize_base_url
 from .testing import discover_models, probe_provider
@@ -7,6 +9,8 @@ __all__ = [
     "GatewayResult",
     "ProviderStore",
     "complete",
+    "create_console_router",
+    "create_provider_router",
     "discover_models",
     "mask_key",
     "model_candidates",
